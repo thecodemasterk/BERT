54d2d0d856a10ba62e8e3032ebb9f3d9b0b3cc72
Karthik Thandapani <46048314+thecodemasterk@users.noreply.github.com>
2020-12-10 14:18:49 +0530

    Update run_squad 2.py
    making changes...

--- TestThat/run_squad 2.py
+++ TestThat/run_squad 2.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors.
+# Copyright 2020 The Google AI Language Team Authors @Karthik Thandapani
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Run BERT on SQuAD 1.1 and SQuAD 2.0."""
+"""Run BERT on SQuAD 2.0 and SQuAD 2.0."""
 
 from __future__ import absolute_import
 from __future__ import division
